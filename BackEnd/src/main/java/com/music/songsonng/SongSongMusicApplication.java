2d538b82026357f8a6fd6ba8df74148fb869329e
이경용 <sleekydz86@icloud.com>
2023-08-31 21:38:01 +0900

    스프링부트 송송뮤직 셋팅
    스프링부트 송송뮤직 셋팅

---
+++ BackEnd/src/main/java/com/music/songsonng/SongSongMusicApplication.java
@@ -0,0 +1,13 @@
+package com.music.songsonng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongSongMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongSongMusicApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/music/songsonng/SongSongMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.songsonng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongSongMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
